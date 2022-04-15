3ff31c0d1821a4ab532c8a10785658d56717640f
shixiongjing <shj40@pitt.edu>
2022-04-15 16:56:17 -0400

    test

--- main.py
+++ main.py
@@ -131,8 +131,7 @@ def autorun(args):
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(0)
 
-    print(args.dataset, str(args.degree_as_tag))
-    graphs, num_classes = load_data(args.dataset, args.degree_as_tag)
+    graphs, num_classes, tagset = load_data(args.dataset, args.degree_as_tag)
 
     ##10-fold cross validation. Conduct an experiment on the fold specified by args.fold_idx.
     train_graphs, test_graphs = separate_data(graphs, args.seed, args.fold_idx)
