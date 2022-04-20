50534184f281802b39508a38777a9d661cc1c84a
shixiongjing <shj40@pitt.edu>
2022-04-20 18:44:56 -0400

    test

--- main.py
+++ main.py
@@ -209,6 +209,7 @@ def main():
     device = torch.device("cuda:" + str(args.device)) if torch.cuda.is_available() else torch.device("cpu")
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(0)
+        print('--Using CUDA--')
 
     graphs, num_classes, tag_count = load_data(args.dataset, args.degree_as_tag)
 
