a5fa8dad916bc0ac0eff0477b361d2880da9dab7
shixiongjing <shj40@pitt.edu>
2022-05-05 10:11:18 -0400

    add one edge attack

--- util.py
+++ util.py
@@ -32,7 +32,6 @@ class S2VGraph(object):
         self.neighbors.append([])
         connections = (x for x in range(0, len(adj_noise)) if adj_noise[x]>0)
         degree_list = []
-        new_edges = []
         for n in connections:
             self.g.add_edge(node_id, n)
             self.neighbors[node_id].append(n)
@@ -45,6 +44,33 @@ class S2VGraph(object):
         for i in range(len(self.g)):
             degree_list.append(len(self.neighbors[i]))
         self.max_neighbor = max(degree_list)
+        self.node_tags.append(tag)
+
+        if S2VGraph.tag2index:
+            self.node_features = torch.zeros(len(self.node_tags), len(S2VGraph.tag2index))
+            self.node_features[range(len(self.node_tags)), [S2VGraph.tag2index[tag] for tag in self.node_tags]] = 1
+        else:
+            print('Error. Fail to find Tag2index')
+
+        return self
+
+    def add_single_edge_noise(self, target_id, tag):
+        node_id = len(self.g)
+        self.g.add_node(node_id)
+
+        # add neighbors
+        self.neighbors.append([])
+        degree_list = []
+        
+        self.g.add_edge(node_id, target_id)
+        self.neighbors[node_id].append(target_id)
+        self.neighbors[target_id].append(node_id)
+
+        edges = [list(pair) for pair in self.g.edges()]
+        edges.extend([[i, j] for j, i in edges])
+        self.edge_mat = torch.LongTensor(edges).transpose(0,1)
+
+        
         self.node_tags.append(tag)
 
         if S2VGraph.tag2index:
